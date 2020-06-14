b03e4859fe35d3a00137ec4acc8b26176229148c
Tim Holman <timothy.w.holman@gmail.com>
2020-06-14 16:29:34 -0400

    fixes for mobile

--- js/header.js
+++ js/header.js
@@ -1,73 +1,90 @@
-var headers = [document.getElementById('back'), document.getElementById('info-tab')] ;
+var headers = [
+    document.getElementById("back"),
+    document.getElementById("info-tab"),
+];
 var headerMouseDown = false;
 var headerToggleTimeOut = [];
-    
-document.addEventListener( 'mousedown', function() {
-    headerMouseDown = true;
-}, false );
-    
-document.addEventListener( 'mouseup', function() {
-    headerMouseDown = false;
-}, false );
-    
-for (var i = 0; i < headers.length; i++ ) {
 
-    headerToggleTimeOut.push( -1 );
-    
-    headers[i].addEventListener('mouseover', function() {
-    
-        if (!headerMouseDown) {
+document.addEventListener(
+    "mousedown",
+    function () {
+        headerMouseDown = true;
+    },
+    false
+);
 
-            interval = clearInterval( interval );
-            
+document.addEventListener(
+    "mouseup",
+    function () {
+        headerMouseDown = false;
+    },
+    false
+);
+
+for (var i = 0; i < headers.length; i++) {
+    headerToggleTimeOut.push(-1);
+
+    headers[i].addEventListener(
+        "mouseover",
+        function () {
+            if (!headerMouseDown) {
+                interval = clearInterval(interval);
+
+                var _this = this;
+
+                clearTimeout(headerToggleTimeOut);
+
+                headerToggleTimeOut[i] = setTimeout(function () {
+                    _this.setAttribute("class", "open");
+                }, 50);
+            }
+        },
+        false
+    );
+
+    headers[i].addEventListener(
+        "mouseout",
+        function () {
             var _this = this;
-            
-            clearTimeout( headerToggleTimeOut );
-            
-            headerToggleTimeOut[i] = setTimeout( function() {
-                _this.setAttribute( 'class', 'open' )
-            }, 50 );
-        }
-                                                 
-    }, false);
-            
-    headers[i].addEventListener('mouseout', function() {
 
-        var _this = this;                    
-        
-        clearTimeout( headerToggleTimeOut );
-        
-        headerToggleTimeOut[i] = setTimeout( function() {
-            _this.setAttribute( 'class', '' )
-        }, 50 );
-            
-    }, false);                   
+            clearTimeout(headerToggleTimeOut);
+
+            headerToggleTimeOut[i] = setTimeout(function () {
+                _this.setAttribute("class", "");
+            }, 50);
+        },
+        false
+    );
 }
 
 function pulseHeader() {
-        
-    if (interval == undefined) { return };    
-    headers[1].setAttribute( 'class', 'highlight');
-    
-    setTimeout( function() {
-        if (interval == undefined) { return };
-        headers[1].setAttribute( 'class', '');
-        
-        setTimeout( function() {
-            if (interval == undefined) { return };
-            headers[1].setAttribute( 'class', 'highlight');
-            
-            setTimeout( function() {
-                if (interval == undefined) { return };
-                headers[1].setAttribute( 'class', '');
-                                             
-            }, 400 );
-            
-        }, 400 );
-        
-    }, 400 ); 
+    if (interval == undefined) {
+        return;
+    }
+    headers[1].setAttribute("class", "highlight");
+
+    setTimeout(function () {
+        if (interval == undefined) {
+            return;
+        }
+        headers[1].setAttribute("class", "");
+
+        setTimeout(function () {
+            if (interval == undefined) {
+                return;
+            }
+            headers[1].setAttribute("class", "highlight");
+
+            setTimeout(function () {
+                if (interval == undefined) {
+                    return;
+                }
+                headers[1].setAttribute("class", "");
+            }, 400);
+        }, 400);
+    }, 400);
 }
 
-var interval = setInterval( function() {
+var interval = setInterval(function () {
     pulseHeader();
-}, 10000 );
+}, 10000);
--- js/texter.js
+++ js/texter.js
@@ -7,11 +7,10 @@
  */
 
 function Texter() {
-
   var _this = this;
 
   // Application variables
-  position = {x: 0, y: window.innerHeight/2};
+  position = { x: 0, y: window.innerHeight / 2 };
   textIndex = 0;
   this.textColor = "#000000";
   this.bgColor = "#ffffff";
@@ -19,160 +18,170 @@ function Texter() {
   this.maxFontSize = 300;
   this.angleDistortion = 0.01;
 
-  this.text = "There was a table set out under a tree in front of the house, and the March Hare and the Hatter were having tea at it: a Dormouse was sitting between them, fast asleep, and the other two were using it as a cushion, resting their elbows on it, and talking over its head. 'Very uncomfortable for the Dormouse,' thought Alice; 'only, as it's asleep, I suppose it doesn't mind.'";
+  var queryString = window.location.search;
+  var urlParams = new URLSearchParams(queryString);
+  var urlText = urlParams.get('text')
+
+  this.text = urlText || 
+    "There was a table set out under a tree in front of the house, and the March Hare and the Hatter were having tea at it: a Dormouse was sitting between them, fast asleep, and the other two were using it as a cushion, resting their elbows on it, and talking over its head. 'Very uncomfortable for the Dormouse,' thought Alice; 'only, as it's asleep, I suppose it doesn't mind.'";
 
   // Drawing Variables
   canvas = null;
   context = null;
-  mouse = {x: 0, y: 0, down: false};
+  mouse = { x: 0, y: 0, down: false };
 
   bgCanvas = null;
   bgContext = null;
 
-  this.initialize = function() {
-    
-    canvas = document.getElementById( 'canvas' );
-    context = canvas.getContext( '2d' );
+  this.initialize = function () {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    
-    canvas.addEventListener('mousemove', mouseMove, false);
-    canvas.addEventListener('mousedown', mouseDown, false);
-    canvas.addEventListener('mouseup',   mouseUp,   false);
-    canvas.addEventListener('mouseout',  mouseUp,  false);
 
-    bgCanvas = document.createElement( 'canvas' );
-    bgContext = bgCanvas.getContext( '2d' );
+    canvas.addEventListener("mousemove", onMove, false);
+    canvas.addEventListener("mousedown", onDown, false);
+    canvas.addEventListener("mouseup", onUp, false);
+    canvas.addEventListener("mouseout", onUp, false);
+
+    canvas.addEventListener("touchstart", onDown, false);
+    canvas.addEventListener("touchmove", onMove, false);
+    canvas.addEventListener("touchend", onUp, false);
+    canvas.addEventListener("touchcancel", onUp, false);
+
+    bgCanvas = document.createElement("canvas");
+    bgContext = bgCanvas.getContext("2d");
     bgCanvas.width = canvas.width;
     bgCanvas.height = canvas.height;
-    _this.setBackground( _this.bgColor );
+    _this.setBackground(_this.bgColor);
 
-    window.onresize = function(event) {
+    window.onresize = function (event) {
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
       bgCanvas.width = window.innerWidth;
       bgCanvas.height = window.innerHeight;
-      _this.setBackground( _this.bgColor );
+      _this.setBackground(_this.bgColor);
       _this.clear();
-    }
+    };
 
     update();
   };
 
-  var mouseMove = function( event ) {
-    mouse.x = event.pageX;
-    mouse.y = event.pageY;
+  var update = function () {
+    requestAnimationFrame(update);
     draw();
   };
 
-  var update = function() {
-    requestAnimationFrame( update );
-    draw();
-  }
+  var draw = function () {
+    if (mouse.down) {
+      var newDistance = distance(position, mouse);
+      var fontSize = _this.minFontSize + newDistance / 2;
 
-  var draw = function() { 
-    if ( mouse.down ) {
-      var newDistance = distance( position, mouse );
-      var fontSize = _this.minFontSize + newDistance/2;
-
-      if ( fontSize > _this.maxFontSize ) {
+      if (fontSize > _this.maxFontSize) {
         fontSize = _this.maxFontSize;
       }
 
       var letter = _this.text[textIndex];
-      var stepSize = textWidth( letter, fontSize );
-      
+      var stepSize = textWidth(letter, fontSize);
+
       if (newDistance > stepSize) {
-        var angle = Math.atan2(mouse.y-position.y, mouse.x-position.x);
-        
+        var angle = Math.atan2(mouse.y - position.y, mouse.x - position.x);
+
         context.font = fontSize + "px Georgia";
-      
+
         context.save();
-        context.translate( position.x, position.y);
-        context.rotate( angle + ( Math.random() * ( _this.angleDistortion * 2 ) - _this.angleDistortion ) );
-        context.fillText(letter,0,0);
+        context.translate(position.x, position.y);
+        context.rotate(
+          angle +
+            (Math.random() * (_this.angleDistortion * 2) -
+              _this.angleDistortion)
+        );
+        context.fillText(letter, 0, 0);
         context.restore();
 
         textIndex++;
-        if (textIndex > _this.text.length-1) {
+        if (textIndex > _this.text.length - 1) {
           textIndex = 0;
         }
-      
+
         position.x = position.x + Math.cos(angle) * stepSize;
         position.y = position.y + Math.sin(angle) * stepSize;
-
       }
     }
   };
 
-  var distance = function( pt, pt2 ){
+  var distance = function (pt, pt2) {
     var xs = 0;
     var ys = 0;
-   
+
     xs = pt2.x - pt.x;
     xs = xs * xs;
-   
+
     ys = pt2.y - pt.y;
     ys = ys * ys;
-   
-    return Math.sqrt( xs + ys );
+
+    return Math.sqrt(xs + ys);
   };
 
-  var mouseDown = function( event ){
+  var onDown = function (event) {
+    const eventObject = event.touches && event.touches.item(0) || event
     mouse.down = true;
-    position.x = event.pageX;
-    position.y = event.pageY;
-  }
+    position.x = eventObject.pageX;
+    position.y = eventObject.pageY;
+    mouse.x = eventObject.pageX;
+    mouse.y = eventObject.pageY;
+  };
 
-  var mouseUp = function( event ){
+  var onUp = function () {
     mouse.down = false;
-  }
+  };
 
-  var textWidth = function( string, size ) {
+  var onMove = function (event) {
+    const eventObject = event.touches && event.touches.item(0) || event
+    mouse.x = eventObject.pageX;
+    mouse.y = eventObject.pageY;
+    draw();
+  };
+
+  var textWidth = function (string, size) {
     context.font = size + "px Georgia";
-    
-    if ( context.fillText ) {
-      return context.measureText( string ).width;
 
-    } else if ( context.mozDrawText) {
-      return context.mozMeasureText( string );
-
+    if (context.fillText) {
+      return context.measureText(string).width;
+    } else if (context.mozDrawText) {
+      return context.mozMeasureText(string);
     }
   };
 
-  this.clear = function() {
+  this.clear = function () {
     canvas.width = canvas.width;
     context.fillStyle = _this.textColor;
-  }
-
-  this.applyNewColor = function( value ) {
-    _this.textColor = value;
-    context.fillStyle = _this.textColor;
-  }
-
-  this.setBackground = function( value ) {
-    _this.bgColor = value;
-    canvas.style.backgroundColor = value;
-
   };
 
-  this.onTextChange = function() {
+  this.applyNewColor = function (value) {
+    _this.textColor = value;
+    context.fillStyle = _this.textColor;
+  };
+
+  this.setBackground = function (value) {
+    _this.bgColor = value;
+    canvas.style.backgroundColor = value;
+  };
+
+  this.onTextChange = function () {
     textIndex = 0;
-  }
+  };
 
-  this.save = function() {
-
+  this.save = function () {
     // Prepare the background canvas's color
-    bgContext.rect( 0, 0, bgCanvas.width, bgCanvas.height );
+    bgContext.rect(0, 0, bgCanvas.width, bgCanvas.height);
     bgContext.fillStyle = _this.bgColor;
     bgContext.fill();
 
     // Draw the front canvas onto the bg canvas
-    bgContext.drawImage( canvas, 0, 0 );
+    bgContext.drawImage(canvas, 0, 0);
 
     // Open in a new window
-    window.open( bgCanvas.toDataURL('image/png'), 'mywindow' );
-
-  }
-
-};
+    window.open(bgCanvas.toDataURL("image/png"), "mywindow");
+  };
+}
